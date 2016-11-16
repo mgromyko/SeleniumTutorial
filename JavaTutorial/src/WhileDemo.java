96c7df7e2ae025d45c037f375252d570fc690965
Slava <Slava@Home>
2016-11-16 21:12:08 +0300

    While, Do While, For, Switch

---
+++ JavaTutorial/src/WhileDemo.java
@@ -0,0 +1,25 @@
+
+public class WhileDemo {
+
+	public static void main(String[] args) {
+
+		int i = 10; 
+		
+		while (i < 20){
+			if (i == 17) {
+				break;
+			}
+			System.out.println(i);
+			i++;		
+			
+		}
+		System.out.println("Outside...");
+		
+		do {
+			System.out.println(i);
+			i++;
+		} while (i < 20);
+		
+	}
+
+}
