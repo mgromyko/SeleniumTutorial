f71a2e5d5cad09bc9d8458a4d040c5153f80e312
m.troegubova <m.troegubova@TROEGUBOVA.atwss.com>
2016-11-24 18:03:57 +0300

    Inheritance demo is added

---
+++ JavaTutorial/src/InheritanceDemo.java
@@ -0,0 +1,14 @@
+
+public class InheritanceDemo {
+
+	public static void main(String[] args) {
+		int speed = 0;
+
+		Cars c1 = new Cars(speed);
+		c1.increaseSpeed();
+		
+		Cars bmw1 = new Cars(speed);
+		bmw1.increaseSpeed();
+	}
+
+}
