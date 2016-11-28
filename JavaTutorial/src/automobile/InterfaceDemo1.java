0411cb58aff8ff0c909ddd47ebcb3d9e77f62cb1
m.troegubova <Maria>
2016-11-28 17:46:40 +0300

    Methods overloading in use (three types)

--- JavaTutorial/src/automobile/InterfaceDemo1.java
+++ JavaTutorial/src/automobile/InterfaceDemo1.java
@@ -3,9 +3,9 @@ package automobile;
 public class InterfaceDemo1 {
 
 	public static void main(String[] args) {
-		CarsInterface myInterface = new CarsInterface() {
-		myInterface.engineStart("6 Cyl", true);
-		}
+		CarsInterface myInterface = new CarsInterface(); //{
+//		myInterface.engineStart("6 Cyl", true);
+//		}
 
 	}
 
