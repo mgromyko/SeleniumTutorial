db1dfb7dfdfb113e9a462348cd92384227fd909d
mgrkomyko <2391108@gmail.com>
2017-02-12 18:14:03 +0300

    Locators class was added to correct folder and some locators are added

---
+++ api-desknet/src/main/com/apidesknet/java/framework/locators/Locators.java
@@ -0,0 +1,28 @@
+package main.com.apidesknet.java.framework.locators;
+
+public class Locators {
+
+//	XPath locators for page title and page description
+	public static String pageApiDeskNetNameLocator = "//div[@id='api-_']/h1";
+	public static String pageDescriptionLocator = "//div[@id='header']/div/p";
+	
+//	Here are described XPath locator for main navigation panel itself and its items in it
+	public static String mainNavigationPanelLocator = "//div[@id='scrollingNav']/ul";
+	public static String descriptionNavigationItemLocator = "//*[@id='scrollingNav']/ul/li[1]/a";
+	public static String authentificationNavigationItemLocator = "//*[@id='scrollingNav']/ul/li[2]/a";
+	public static String groupNavigationItemLocator = "//*[@id='scrollingNav']/ul/li[15]/a";
+	public static String attachmentNavigationItemLocator = "//*[@id='scrollingNav']/ul/li[18]/a";
+	public static String formatNavigationItemLocator = "//*[@id='scrollingNav']/ul/li[22]/a";
+	public static String userNavigationItemLocator = "//*[@id='scrollingNav']/ul/li[25]/a";
+	public static String taskContentNavigationItemLocator = "//*[@id='scrollingNav']/ul/li[27]/a";
+	public static String pubStatusNavigationItemLocator = "//*[@id='scrollingNav']/ul/li[31]/a";
+	public static String platformNavigationItemLocator = "//*[@id='scrollingNav']/ul/li[34]/a";	
+	public static String categoryNavigationItemLocator = "//*[@id='scrollingNav']/ul/li[37]/a";	
+	public static String typeNavigationItemLocator = "//*[@id='scrollingNav']/ul/li[41]/a";	
+	public static String supportNavigationItemLocator = "//*[@id='scrollingNav']/ul/li[45]/a";	
+	
+	
+//	public static String platformNavigationItemLocator = "//*[@id='scrollingNav']/ul/li[34]/a";	
+//	public static String platformNavigationItemLocator = "//*[@id='scrollingNav']/ul/li[34]/a";	
+	
+}
